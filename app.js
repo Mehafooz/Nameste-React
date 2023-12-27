d6bf26e1d74c24cc4e9cf96159b45052780f5c9f
Mehafooz <mehafoozalikhan@gmail.com>
2023-12-27 22:44:18 +0530

    ep-03

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ import ReactDOM from "react-dom/client";
 // import React from "_./node_modules/react";
 
 
-const heading = React.createElement("h2",{id:"heading"},"Namaste React injected by react..");
-
+const heading = React.createElement("h2",{id:"heading"},"Namaste React injected by react.."); //it will return the react ele
+const Jsxheading = () => (<h1>Hi jsx react</h1> )//js engine dont unsdertsand iy ,but prcale is transpiled before it reaches the js engine,indirectly done by parcel using babel
+// it is also sme react ele
+//so it is transpiled to react ele
+// so behind teh scene jsx is reactncreateele
 const parent = React.createElement("div",{id:"parent"},
 React.createElement("div",{id:"children"},
 React.createElement("h2",{id:"heading"},"Namaste React injected by react..")));
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent)
+const ReactFuncComp = () =>(
+    <div>
+        <Jsxheading />
+        <h1> react func comp</h1>
+    </div>
+)
+root.render(<ReactFuncComp />)
